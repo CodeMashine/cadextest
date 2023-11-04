e17d4dac80be023079a27ccfbd5605c857fe4950
Peter Mashin <petemashine@gmail.com>
2023-11-04 19:03:48 +0300

    feat: prepare to deploy

--- src/types/types.ts
+++
@@ -1,9 +0,0 @@
-interface MagicValues<T> {
-  [string: string]: T;
-}
-
-type vector = number[];
-type triangle = vector[];
-type coords = triangle[];
-
-export { MagicValues, coords, triangle, vector };
